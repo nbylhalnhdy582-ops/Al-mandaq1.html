f6488e7441f292cb4a1b917540f9a6dd01858b23
nbylhalnhdy582-ops <nbylhalnhdy582@gmail.com>
2026-03-05 03:11:43 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('nav ul');
+    menu.classList.toggle('show');
+}
+
+function showMessage() {
+    alert("أهلاً بك في مدينة المندق 🌿");
+}
